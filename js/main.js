882c5b8649c85d2960f3bfa4cded5dabd05966d3
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-11-05 13:38:12 +0100

    Basics

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const images = [
+    "img/01.webp",
+    "img/02.webp",
+    "img/03.webp",
+    "img/04.webp",
+    "img/05.webp"
+];
+let currentImgIndex = 0;
+
+const sliderImgEl = document.querySelector(".slider-img")
+const buttonNext = document.createElement("div")
