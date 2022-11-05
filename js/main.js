d460b16f68047acf9b164c4a4cc925333f54fb56
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-11-05 16:37:13 +0100

    Base Exercise completed!

--- js/main.js
+++ js/main.js
@@ -7,5 +7,47 @@ const images = [
 ];
 let currentImgIndex = 0;
 
+const btnContainerEl = document.querySelector(".buttons-container")
+
 const sliderImgEl = document.querySelector(".slider-img")
-const buttonNext = document.createElement("div")
+sliderImgEl.src = images [ currentImgIndex ];
+
+// Bottone top per andare indietro
+const prevBtnEl = document.createElement("button")
+prevBtnEl.classList.add("prev-btn")
+prevBtnEl.innerHTML = `<i class="fa-solid fa-chevron-up"></i>`
+
+// Bottone bottom per andare avanti
+const nextBtnEl = document.createElement("button")
+nextBtnEl.classList.add("next-btn")
+nextBtnEl.innerHTML = `<i class="fa-solid fa-chevron-down"></i>`
+console.log(prevBtnEl, nextBtnEl);
+
+// Inserisco i buttons nel loro container
+btnContainerEl.appendChild(prevBtnEl)
+btnContainerEl.appendChild(nextBtnEl)
+
+// Funzione sul next-button per scrollare in avanti
+nextBtnEl.addEventListener( "click", function () {
+    currentImgIndex++;
+    const lastIndex = images.length - 1;
+    console.log(currentImgIndex);
+
+    if ( currentImgIndex > lastIndex ) {
+        currentImgIndex = 0;
+      }
+      console.log(currentImgIndex);    
+      sliderImgEl.src = images[ currentImgIndex ];
+})
+
+// Funzione sul prev-button per scrollare in avanti
+prevBtnEl.addEventListener( "click", function () {
+    currentImgIndex--;
+    console.log(currentImgIndex);
+
+    if ( currentImgIndex < 0 ) {
+        currentImgIndex = 4;
+      }
+      console.log(currentImgIndex);    
+      sliderImgEl.src = images[ currentImgIndex ];
+})
